7ea866ab8bb71e18624c3ed908c827b2b57d5126
Popochounet <adrien.audiard@epitech.eu>
2024-10-09 18:18:31 +0200

    updated Network(Snapshot): cleaner version (need to be updated later with network coming enhancement)

--- include/GEngine/interface/network/systems/Snapshot.hpp
+++ include/GEngine/interface/network/systems/Snapshot.hpp
@@ -15,61 +15,61 @@
 #include "GEngine/libdev/systems/events/MainLoop.hpp"
 #include "GEngine/libdev/systems/events/Native.hpp"
 
+#include "GEngine/interface/network/systems/NetworkComponent.hpp"
+
 #define MAX_SNAPSHOT 60
 
-namespace Network
-{
-    class NetClient;
+namespace Network {
+class NetClient;
 } // namespace Network
 
 namespace gengine::interface::network::system {
 
-// class SnapshotClient {
-// public:
-//     SnapshotClient(std::shared_ptr<Network::NetClient> client, uint64_t firsSnapshotId)
-//         : m_client(client), m_firsSnapshotId(firsSnapshotId) {
-//     }
+class SnapshotClient {
+public:
+    SnapshotClient(std::shared_ptr<Network::NetClient> client, uint64_t firsSnapshotId)
+        : m_client(client)
+        , m_firsSnapshotId(firsSnapshotId) {
+    }
 
-//     std::shared_ptr<Network::NetClient> getNet(void) const {
-//         return m_client;
-//     }
+    std::shared_ptr<Network::NetClient> getNet(void) const {
+        return m_client;
+    }
 
-//     uint64_t getSnapshotId(void) const {
-//         return m_firsSnapshotId;
-//     }
+    uint64_t getSnapshotId(void) const {
+        return m_firsSnapshotId;
+    }
 
-//     void setSnapshotId(uint64_t id) {
-//         m_firsSnapshotId = id;
-//     }
+    void setSnapshotId(uint64_t id) {
+        m_firsSnapshotId = id;
+    }
 
-// private:
-//     std::shared_ptr<Network::NetClient> m_client;
-//     uint64_t m_firsSnapshotId;
-// };
+private:
+    std::shared_ptr<Network::NetClient> m_client;
+    uint64_t m_firsSnapshotId;
+};
 
 class Snapshot : public System<Snapshot> {
 public:
     using snapshot_t = BaseEngine::world_t;
-    using tools_t = BaseEngine::world_tools_t;
     using snapshots_t = std::array<snapshot_t, MAX_SNAPSHOT>;
 
-    Snapshot(const snapshot_t &currentWorld, const tools_t &tools)
-        : m_currentWorld(currentWorld), m_tools(tools) {
-    }
+    Snapshot(const snapshot_t &currentWorld);
 
     void init(void) override;
     void onStartEngine(gengine::system::event::StartEngine &);
     void onMainLoop(gengine::system::event::MainLoop &);
 
-    // void registerClient(std::shared_ptr<Network::NetClient> client);
+    void registerClient(std::shared_ptr<Network::NetClient> client);
     void createSnapshots(void);
-    void deltaDiff(void);
+    void getAndSendDeltaDiff(void);
 
 private:
     const snapshot_t &m_currentWorld;
     snapshot_t m_dummySnapshot;
-    const tools_t &m_tools;
-    // std::vector<std::pair<SnapshotClient,snapshots_t>> m_clientSnapshots;
+    std::vector<std::pair<SnapshotClient, snapshots_t>> m_clientSnapshots;
     uint64_t m_currentSnapshotId = -1;
+
+    std::vector<ecs::component::component_info_t> getDeltaDiff(const snapshot_t &snap1, const snapshot_t &snap2) const;
 };
 } // namespace gengine::interface::network::system
--- source/GEngine/interface/network/systems/Snapshot.cpp
+++ source/GEngine/interface/network/systems/Snapshot.cpp
@@ -11,92 +11,98 @@
 #include "GEngine/net/net.hpp"
 #include "GEngine/net/net_client.hpp"
 
-struct ComponentNetwork {
-    uint64_t entity;
-    uint16_t type;
-    uint16_t size;
-};
-
 namespace gengine::interface::network::system {
+Snapshot::Snapshot(const snapshot_t &currentWorld)
+    : m_currentWorld(currentWorld)
+    , m_dummySnapshot(currentWorld) { // TODO dummy must created after components registered
+}
+
 void Snapshot::init(void) {
     subscribeToEvent<gengine::system::event::StartEngine>(&Snapshot::onStartEngine);
     subscribeToEvent<gengine::system::event::MainLoop>(&Snapshot::onMainLoop);
 }
 
 void Snapshot::onStartEngine(gengine::system::event::StartEngine &e) {
-    /* todo : faudrait renommer le nom du système, ça colle plus à serveur ducoup */
     Network::NET::initServer(*this);
-    m_dummySnapshot = m_currentWorld;
 }
 
 void Snapshot::onMainLoop(gengine::system::event::MainLoop &e) {
     m_currentSnapshotId++;
-    // createSnapshots();
-    // deltaDiff();
+    createSnapshots();
+    getAndSendDeltaDiff();
 
     // /* todo:  ça serait pas vraiment snapshot, plus un systeme network donc */
-    // Network::NET::sleep(300);
+    Network::NET::sleep(300);
 }
 
 /* todo warning : mutex please */
-// void Snapshot::registerClient(std::shared_ptr<Network::NetClient> client) {
-//     m_clientSnapshots.push_back(std::make_pair<>(SnapshotClient(client, m_currentSnapshotId), snapshots_t()));
-//     m_clientSnapshots.at(m_clientSnapshots.size() - 1).second[m_currentSnapshotId % MAX_SNAPSHOT] = m_dummySnapshot;
-//     std::cout << "c:" << m_currentSnapshotId << std::endl;
-// }
+void Snapshot::registerClient(std::shared_ptr<Network::NetClient> client) {
+    m_clientSnapshots.push_back(std::make_pair<>(SnapshotClient(client, m_currentSnapshotId), snapshots_t()));
+    m_clientSnapshots[m_clientSnapshots.size() - 1].second[m_currentSnapshotId % MAX_SNAPSHOT] = m_dummySnapshot;
+}
 
-// void Snapshot::createSnapshots(void) {
-//     for (auto &[client, snap] : m_clientSnapshots) {
-//         if (client.getNet()->isDisconnected()) { /* thread safe way of deleting a client from genengine */
-//             m_clientSnapshots.erase(std::remove_if(m_clientSnapshots.begin(), m_clientSnapshots.end(),
-//                 [&client](const auto &pair) { return pair.first.getNet() == client.getNet(); }), m_clientSnapshots.end());
-//             continue;
-//         }
-//         snap[m_currentSnapshotId % MAX_SNAPSHOT] = m_currentWorld;
-//     }
-// }
+void Snapshot::createSnapshots(void) {
+    for (auto &[client, snap] : m_clientSnapshots) {
+        if (client.getNet()->isDisconnected()) { /* thread safe way of deleting a client from genengine */
+            m_clientSnapshots.erase(
+                std::remove_if(m_clientSnapshots.begin(), m_clientSnapshots.end(),
+                               [&client](const auto &pair) { return pair.first.getNet() == client.getNet(); }),
+                m_clientSnapshots.end());
+            continue;
+        }
+        snap[m_currentSnapshotId % MAX_SNAPSHOT] = m_currentWorld;
+    }
+}
 
-/* ADRIEN /!\ : we need to ensure that we send something, even when 0 bytes */
-void Snapshot::deltaDiff(void) {
+void Snapshot::getAndSendDeltaDiff(void) {
     auto &server = Network::NET::getServer();
 
-    // for (auto &[client, snapshots] : m_clientSnapshots) {
-    //     auto lastReceived = client.getNet()->getChannel().getLastACKPacketId();
-    //     auto lastId = client.getSnapshotId() + lastReceived;
+    for (auto &[client, snapshots] : m_clientSnapshots) {
+        auto lastReceived = client.getNet()->getChannel().getLastACKPacketId();
+        auto lastId = client.getSnapshotId() + lastReceived;
+        auto diff = m_currentSnapshotId - lastId;
+        std::cout << "diff: " << diff << " | m_currentSnapshotId: " << m_currentSnapshotId << " last id: " << lastId
+                  << " UDP Last ACK: " << lastReceived << std::endl;
 
-    //     auto &current = snapshots[m_currentSnapshotId % MAX_SNAPSHOT];
+        auto &current = snapshots[m_currentSnapshotId % MAX_SNAPSHOT];
+        auto &last = diff > MAX_SNAPSHOT ? m_dummySnapshot : snapshots[lastId % MAX_SNAPSHOT];
 
-    //     auto diff = m_currentSnapshotId - lastId;
-    //     std::cout << "diff: " << diff << " | m_currentSnapshotId: " << m_currentSnapshotId << " last id: " << lastId << " UDP Last ACK: " << lastReceived << std::endl;
-    //     std::vector<ecs::component::component_info_t> v;
-    //     if (diff > MAX_SNAPSHOT || !diff) {
-    //         // diff = MAX_SNAPSHOT; /* todo : find dummy snapshot (all 0) to send all */
-    //         v = ecs::component::deltaDiff(current, m_tools, m_dummySnapshot);
-    //     } else {
-    //         auto &last = snapshots[lastId % MAX_SNAPSHOT]; // TODO check which world client is using
-    //         v = ecs::component::deltaDiff(current, m_tools, last);
-    //     }
-    //     // try {
-    //     // } catch (const std::exception &e) {
-    //     // }
-    //     uint64_t nb_component = v.size();
-    //     if (!nb_component)
-    //         break;
-    //     Network::UDPMessage msg(true, Network::SV_SNAPSHOT);
-    //     msg.setAck(true);
-    //     msg.appendData(nb_component);
-    //     for (auto &[entity, type, any]: v) {
-    //         auto &[d, s, c, info] = m_tools.find(type)->second;
-    //         ComponentNetwork cn = {.entity = entity, .type = info.first, .size = info.second}; /* todo : found the size of the entity as well as type !!!! */
-    //         // strcpy(c.type, type.name());
-    //         std::cout << nb_component << " " << info.second << std::endl;
-    //         msg.appendData(cn);
-    //     }
+        std::vector<ecs::component::component_info_t> deltaDiff = getDeltaDiff(current, last);
+        if (diff > MAX_SNAPSHOT || !diff)
+            deltaDiff = getDeltaDiff(current, m_dummySnapshot);
+        else
+            deltaDiff = getDeltaDiff(current, snapshots[lastId % MAX_SNAPSHOT]);
 
-    //     if (!server.isRunning())
-    //         continue;
+        uint64_t nb_component = deltaDiff.size();
+        Network::UDPMessage msg(true, Network::SV_SNAPSHOT);
+        msg.setAck(true);
+        msg.appendData(nb_component);
+        for (auto &[entity, type, any] : deltaDiff) {
+            NetworkComponent c(entity, getComponentId(type),
+                               getComponentSize(type)); /* todo : found the size of the entity as well as type !!!! */
+            msg.appendData(c);
+            msg.writeData(toVoid(type, any), c.size);
+        }
 
-    //     server.sendToClient(*client.getNet(), msg);
-    // }
+        if (!server.isRunning())
+            continue;
+
+        server.sendToClient(*client.getNet(), msg);
+    }
+}
+
+std::vector<ecs::component::component_info_t> Snapshot::getDeltaDiff(const snapshot_t &snap1,
+                                                                     const snapshot_t &snap2) const {
+    std::vector<ecs::component::component_info_t> diff;
+
+    for (auto &[type, any] : snap1) {
+        if (!snap2.contains(type))
+            THROW_ERROR(
+                "the 2 world do not contain the same component - verify your component registry order"); // big error
+
+        for (auto [e, t, c] : compareComponents(type, any, snap2.find(type)->second))
+            diff.emplace_back(e, t, c);
+    }
+    return diff;
 }
 } // namespace gengine::interface::network::system
