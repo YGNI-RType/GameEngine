40e40daf78684a86a344f99f78d02ca971f7a1f2
Popochounet <adrien.audiard@epitech.eu>
2024-10-10 22:07:29 +0200

    updated workflows(beutify): runs on fedora for a more recent clang format version

--- beautify.sh
+++ beautify.sh
@@ -7,11 +7,11 @@ fi
 # Get the clang-format version
 CLANG_FORMAT_VERSION=$(clang-format --version)
 
-# Check if the version is 14.x.x
-if [[ "$CLANG_FORMAT_VERSION" != *"14."* ]]; then
-    echo "Error: clang-format version 14.x.x is required. Found: $CLANG_FORMAT_VERSION"
-    exit 1
-fi
+# # Check if the version is 14.x.x
+# if [[ "$CLANG_FORMAT_VERSION" != *"14."* ]]; then
+#     echo "Error: clang-format version 14.x.x is required. Found: $CLANG_FORMAT_VERSION"
+#     exit 1
+# fi
 
 make clean -C build || true
 rm -r build/CMakeFiles || true
