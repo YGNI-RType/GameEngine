a908fdf289515eec586c4698a45fe5e95fac1ece
b3W <basile.fouquet@epitech.eu>
2024-10-11 17:57:54 +0200

    added poc: r-type
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

---
+++ r-type/build/CMakeFiles/r-type_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r-type_client.dir/sources/Start.cpp.o"
+  "CMakeFiles/r-type_client.dir/sources/Start.cpp.o.d"
+  "CMakeFiles/r-type_client.dir/sources/main.cpp.o"
+  "CMakeFiles/r-type_client.dir/sources/main.cpp.o.d"
+  "r-type_clientd"
+  "r-type_clientd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r-type_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ r-type/compilse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cmake --build build --target GEngine_static
+
+cd r-type
+
+cmake --build build
+
+cd build
+
+echo STARTING SERVE RNOW
+./r-type_serverd
