d54f9ac984f37700ad639c780766465f7e924e3d
Popochounet <adrien.audiard@epitech.eu>
2024-10-11 10:29:51 +0200

    updated beautify.sh: clang format exclude gitignored files

--- beautify.sh
+++ beautify.sh
@@ -7,12 +7,5 @@ fi
 # Get the clang-format version
 CLANG_FORMAT_VERSION=$(clang-format --version)
 
-# # Check if the version is 14.x.x
-# if [[ "$CLANG_FORMAT_VERSION" != *"14."* ]]; then
-#     echo "Error: clang-format version 14.x.x is required. Found: $CLANG_FORMAT_VERSION"
-#     exit 1
-# fi
-
-make clean -C build || true
-rm -r build/CMakeFiles || true
-clang-format --verbose -i $(find . -name "*.hpp" -o -name "*.cpp" -o -name "*.inl")
+# Run clang format for every .hpp .cpp and .inl files except gitignored ones
+clang-format --verbose -i $(git ls-files --others --exclude-standard --cached | grep -E '\.hpp$|\.cpp$|\.inl$')
