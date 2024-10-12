9af5cff267088cebb620d60db8c0711dc4b24a82
b3W <basile.fouquet@epitech.eu>
2024-10-12 20:27:34 +0200

    added interface: network2
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- include/GEngine/interface/network/systems/ClientEventPublisher.hpp
+++ include/GEngine/interface/network/systems/ClientEventPublisher.hpp
@@ -71,7 +71,7 @@ private:
             m_eventCount++;
 
             // temp but PLEASE IN THE FUTURE ADD A CLOCK
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
         });
         id++;
     }
