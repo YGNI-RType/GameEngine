97e3ae367f50afd7ee925d73879582c8a430e1b9
b3W <basile.fouquet@epitech.eu>
2024-10-13 23:30:03 +0200

    added cmake: boost-uuid
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

--- include/GEngine/interface/components/RemoteDriver.hpp
+++ include/GEngine/interface/components/RemoteDriver.hpp
@@ -19,59 +19,26 @@ namespace gengine::interface::component {
     class RemoteDriver: public gengine::Component<RemoteDriver> {
     public:
         // Constructor - Generates a new UUID upon object creation
-        RemoteDriver() {
-            generateUUID();
-        }
+        RemoteDriver();
 
         // Copy constructor
-        RemoteDriver(const RemoteDriver& other) {
-            m_uuid = other.getUUIDBytes();
-        }
+        RemoteDriver(const RemoteDriver& other);
 
         // Assignment operator
-        RemoteDriver& operator=(const RemoteDriver& other) {
-            if (this != &other) {
-                m_uuid = other.getUUIDBytes();
-            }
-            return *this;
-        }
+        RemoteDriver& operator=(const RemoteDriver& other);
 
         // Overloading the == operator to compare based on UUID
-        bool operator==(const RemoteDriver& other) const {
-            return m_uuid == other.getUUIDBytes();
-        }
+        bool operator==(const RemoteDriver& other) const;
 
         // Getter for the UUID as a string (hexadecimal format)
-        std::string getUUIDString() const {
-            return boost::uuids::to_string(m_uuid);
-        }
+        std::string getUUIDString() const;
 
         // Getter for the raw UUID bytes (for network transmission)
-        const boost::uuids::uuid& getUUIDBytes() const {
-            return m_uuid;
-        }
-
+        const boost::uuids::uuid& getUUIDBytes() const;
     private:
         boost::uuids::uuid m_uuid;
 
         // Method to generate a random UUID using Boost
-        void generateUUID() {
-            boost::uuids::random_generator generator;
-            m_uuid = generator();
-        }
-    };
-}
-
-namespace std {
-    template <>
-    struct hash<gengine::interface::component::RemoteDriver> {
-        std::size_t operator()(const gengine::interface::component::RemoteDriver& driver) const {
-            const boost::uuids::uuid& uuid = driver.getUUIDBytes();
-            std::size_t hash_value = 0;
-            for (auto byte : uuid) {
-                hash_value ^= std::hash<uint8_t>{}(byte) + 0x9e3779b9 + (hash_value << 6) + (hash_value >> 2);
-            }
-            return hash_value;
-        }
+        void generateUUID();
     };
 }
---
+++ source/GEngine/interface/components/RemoteDriver.cpp
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2024
+** GameEngine
+** File description:
+** RemoteDriver.cpp
+*/
+
+#include "GEngine/interface/components/RemoteDriver.hpp"
+
+namespace gengine::interface::component {
+
+RemoteDriver::RemoteDriver() {
+    generateUUID();
+}
+
+// Copy constructor
+RemoteDriver::RemoteDriver(const RemoteDriver& other) {
+    m_uuid = other.getUUIDBytes();
+}
+
+// Assignment operator
+RemoteDriver& RemoteDriver::operator=(const RemoteDriver& other) {
+    if (this != &other) {
+        m_uuid = other.getUUIDBytes();
+    }
+    return *this;
+}
+
+// Overloading the == operator to compare based on UUID
+bool RemoteDriver::operator==(const RemoteDriver& other) const {
+    return m_uuid == other.getUUIDBytes();
+}
+
+// Getter for the UUID as a string (hexadecimal format)
+std::string RemoteDriver::getUUIDString() const {
+    return boost::uuids::to_string(m_uuid);
+}
+
+// Getter for the raw UUID bytes (for network transmission)
+const boost::uuids::uuid& RemoteDriver::getUUIDBytes() const {
+    return m_uuid;
+}
+
+void RemoteDriver::generateUUID() {
+    boost::uuids::random_generator generator;
+    m_uuid = generator();
+}
+}
