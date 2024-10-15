8a1eb1d7ca49d9cc57f687b88dee4dcb4ee460da
Popochounet <adrien.audiard@epitech.eu>
2024-10-15 22:00:56 +0200

    style GEngine: clang format

--- include/GEngine/libdev/systems/AutoKiller.hpp
+++ include/GEngine/libdev/systems/AutoKiller.hpp
@@ -12,8 +12,7 @@
 #include "GEngine/libdev/systems/events/GameLoop.hpp"
 
 namespace gengine::system {
-class AutoKiller
-    : public System<AutoKiller, component::SpanLife> {
+class AutoKiller : public System<AutoKiller, component::SpanLife> {
 public:
     void init(void) override;
     void onGameLoop(event::GameLoop &);
