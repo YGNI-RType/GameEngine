288a761d547220d71787c4d630583e7f262b2cb1
Popochounet <adrien.audiard@epitech.eu>
2024-10-15 07:45:47 +0200

    added system(AnimationManager (to complete) & Animate refacto): refacto Animate with new Animation component

--- source/GEngine/libdev/systems/driver/output/Animate.cpp
+++ source/GEngine/libdev/systems/driver/output/Animate.cpp
@@ -16,24 +16,49 @@ void Animate::onGameLoop(gengine::system::event::GameLoop &e) {
     auto &animations = getComponents<component::driver::output::Animation>();
     auto &sprites = getComponents<component::driver::output::Sprite>();
 
+    auto &animMan = getSystem<AnimationManager>();
     for (auto [entity, anim, sprite] : Zip(animations, sprites)) {
         anim.currentTime += e.deltaTime / 1000.f;
-        int newFrame = anim.currentTime / anim.frameDuration;
-        if (!(newFrame - anim.currentFrame))
+        if (anim.currentTime < anim.frameDuration)
             continue;
-        if (newFrame >= anim.nbFrames) {
-            if (anim.looping) {
-                newFrame %= anim.nbFrames;
-                anim.currentTime = 0.f;
-            } else {
-                newFrame = anim.nbFrames - 1;
+        anim.currentTime -= anim.frameDuration; // while ?
+        auto &track = animMan.getAnimationTrack(anim.trackName);
+        int currentframe = track.frames[anim.currentFrameIndex];
+        switch (anim.getPlaybackMode()) {
+        case ATrack::PlaybackMode::Forward:
+            if (anim.currentFrameIndex == track.frames.size() - 1) {
+                if (track.looping) {
+                    anim.currentFrameIndex = 0;
+                } else {
+                    anim.currentFrameIndex = track.frames.size() - 1;
+                }
+            } else
+                anim.currentFrameIndex++;
+            break;
+        case ATrack::PlaybackMode::Reverse:
+            if (anim.currentFrameIndex == 0) {
+                if (track.looping) {
+                    anim.currentFrameIndex = track.frames.size() - 1;
+                } else {
+                    anim.currentFrameIndex = 0;
+                }
+            } else
+                anim.currentFrameIndex--;
+            break;
+        case ATrack::PlaybackMode::Idling:
+            if (anim.currentFrameIndex < track.idleFrameIdx) {
+                anim.currentFrameIndex++;
+            } else if (anim.currentFrameIndex > track.idleFrameIdx) {
+                anim.currentFrameIndex--;
             }
+            break;
+        default:
+            break;
         }
-        int delta = newFrame - anim.currentFrame;
 
+        int delta = track.frames[anim.currentFrameIndex] - currentframe;
         sprite.src.x += delta * sprite.src.width;
-
-        anim.currentFrame = newFrame;
     }
 }
+
 } // namespace gengine::system::driver::output
