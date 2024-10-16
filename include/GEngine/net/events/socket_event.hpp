fa873d6319ad6ac3cda51a0ad83dc15d50fdb61e
thomas-pommier-epi <thomas.pommier@epitech.eu>
2024-10-16 02:54:42 +0200

    fixed network: re adding the socket pair for windows

--- include/GEngine/net/events/socket_event.hpp
+++ include/GEngine/net/events/socket_event.hpp
@@ -10,10 +10,10 @@
 #include "../net_socket.hpp"
 
 #ifdef _WIN32
-#else
-#ifndef __APPLE__
+#include <atomic>
+#elif defined(__linux__)
 #include <sys/eventfd.h>
-#endif
+#else
 #include <unistd.h>
 #endif
 
@@ -38,6 +38,9 @@ public:
 #ifdef HAS_NOT_EVENTFD
 private:
     SOCKET m_sockConnect = -1;
+    #ifdef _WIN32
+        std::atomic_bool m_hasRead = true;
+    #endif
 #endif
 };
 } // namespace Network::Event
--- source/GEngine/net/events/net_socket_event.cpp
+++ source/GEngine/net/events/net_socket_event.cpp
@@ -7,25 +7,86 @@
 
 #include "GEngine/net/events/socket_event.hpp"
 
-#ifdef HAS_NOT_EVENTFD
 #include <stdexcept>
+
+#ifdef HAS_NOT_EVENTFD
+#ifdef __unix__
+#define closesocket close
 #include <unistd.h>
+#else
+#include <cstring>
+#endif
 #endif
 
 namespace Network::Event {
 
 SocketEvent::SocketEvent() {
 #ifdef HAS_NOT_EVENTFD
+#if defined(__APPLE__) || defined(__unix__)
     int pipefd[2];
     if (pipe(pipefd) == -1)
         throw std::runtime_error("Failed to create pipe");
 
     int flags = fcntl(pipefd[1], F_GETFL, 0);
     fcntl(pipefd[1], F_SETFL, flags | O_NONBLOCK);
+#if __linux__
     int pipe_sz = fcntl(pipefd[1], F_SETPIPE_SZ, 1);
+#endif
 
     m_sock = pipefd[0];
     m_sockConnect = pipefd[1];
+#else
+    ASocket::initLibs();
+    m_sockConnect = socket(AF_INET, SOCK_STREAM, 0);
+    if (m_sockConnect == INVALID_SOCKET)
+        throw std::runtime_error("Failed to create socket");
+
+    auto listenSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSock == INVALID_SOCKET)
+        throw std::runtime_error("Failed to create socket");
+
+    unsigned int opt = 1;
+    setsockopt(listenSock, SOL_SOCKET, SO_EXCLUSIVEADDRUSE, (char *)&opt, (socklen_t)sizeof(opt));
+
+    struct sockaddr_in addr;
+    std::memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port = 0;
+
+    if (bind(listenSock, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        closesocket(listenSock);
+        throw std::runtime_error("Failed to bind socket");
+    }
+
+    int len = sizeof(addr);
+    if (getsockname(listenSock, (struct sockaddr *)&addr, &len) == -1) {
+        closesocket(listenSock);
+        throw std::runtime_error("Failed to bind socket");
+    }
+
+    if (listen(listenSock, 1) == -1) {
+        closesocket(listenSock);
+        throw std::runtime_error("Failed to bind socket");
+    }
+
+    if (connect(m_sockConnect, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        closesocket(m_sockConnect);
+        throw std::runtime_error("Failed to connect to socket");
+    }
+
+    /** warning : todo https://github.com/python/cpython/pull/122134/files
+     * avec cette méthode, on peut se faire hijack cette connexion. même si osef ça peut faire chier
+     */
+
+    m_sock = accept(listenSock, nullptr, nullptr);
+    if (m_sock == -1) {
+        closesocket(m_sock);
+        throw std::runtime_error("Failed to connect to socket");
+    }
+
+    closesocket(listenSock);
+#endif
 #else
     m_sock = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
     if (m_sock == -1)
@@ -37,7 +98,7 @@ SocketEvent::SocketEvent() {
 SocketEvent::~SocketEvent() {
 #ifdef HAS_NOT_EVENTFD
     if (m_sockConnect != -1)
-        close(m_sockConnect);
+        closesocket(m_sockConnect);
 #endif
 }
 
@@ -63,7 +124,14 @@ SocketEvent &SocketEvent::operator=(SocketEvent &&other) {
 void SocketEvent::signal() {
 #ifdef HAS_NOT_EVENTFD
     char buf[1] = {0};
-    size_t sze = write(m_sockConnect, buf, sizeof(buf));
+#ifdef _WIN32
+    if (!m_hasRead)
+        return;
+    send(m_sockConnect, buf, sizeof(buf), 0);
+    m_hasRead = false;
+#else
+    write(m_sockConnect, buf, sizeof(buf));
+#endif
 #else
     eventfd_write(m_sock, 1);
 #endif
@@ -72,7 +140,14 @@ void SocketEvent::signal() {
 void SocketEvent::wait() {
 #ifdef HAS_NOT_EVENTFD
     char buf[1];
+#ifdef _WIN32
+    if (!m_hasRead)
+        return;
+    recv(m_sockConnect, buf, sizeof(buf), 0);
+    m_hasRead = true;
+#else
     read(m_sock, buf, sizeof(buf));
+#endif
 #else
     eventfd_t c;
     eventfd_read(m_sock, &c);
